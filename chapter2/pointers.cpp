35f57e83cf8262941cfc9fcf352bf6a3c9953948
shubharthaksangharsha <shubharthaksangharsha@gmai.com>
2021-06-10 16:20:25 +0530

    why same address ?

--- chapter2/pointers.cpp
+++ chapter2/pointers.cpp
@@ -13,8 +13,24 @@ int main(){
   int *p2 = 0 ;
   int *p3 = NULL;
   cout<<"*********"<<endl;
-  int var1 = 25 , *pointer = nullptr; 
-  pointer = &var1 ;
+  // int var1 = 25 , *pointer = nullptr, *pointer2 = NULL; 
+  // pointer = &var1 ;
+  // cout << *pointer<<endl;
+  // pointer2 = pointer;
+  // pointer2 = 0 ;
+  // cout<< pointer2<<endl<<*pointer<<endl<<pointer;
+  int i = 42;
+  int *pi = 0 ;
+  int *pi2 = &i;
+  int *pi3;
+  cout<<"pi2 = "<< pi2<<endl<<"pi  = " << pi << endl;
+  cout<<pi3<<endl;
+  pi3= pi2;
+  cout<<pi3<<endl;
+  pi=&i;
+  *pi3=0;
+  cout<<*pi;
+  cout<<endl<<pi<<endl<<pi2<<endl<<pi3;
   
   return 0;
   
