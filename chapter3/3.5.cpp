e9e982e146a45446905cc7dc7ff1b0c081f759f7
shubharthaksangharsha <shubharthaksangharsha@gmai.com>
2021-06-12 14:29:11 +0530

    3.5.cpp fixed

--- chapter3/3.5.cpp
+++ chapter3/3.5.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main(){
   string input ,total;
   while(getline(cin,input)){
-    total= total + ' ' + input<<endl;;
+    total= total + ' ' + input;
   }
-  cout<<total;
+  cout<<total<<endl;
   return 0;
 }
