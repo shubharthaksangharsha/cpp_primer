beb25e838c7d7ad555f8c57f07ac49451ce0dc27
shubharthaksangharsha <shubharthaksangharsha@gmai.com>
2021-06-12 17:12:30 +0530

    Added string subscript and for-ranged exercises

---
+++ chapter3/3.10.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using std :: cout ;
+using std :: cin ;
+using std :: endl;
+using std :: string;
+
+int main(){
+  string name ;
+  getline(cin, name ) ;
+
+  for(auto c : name ) {
+    if(ispunct(c)){
+      c= ' ';
+      cout<<c;
+    }
+    else{
+      cout<<c;
+    }
+  }
+
+  return 0;
+}
