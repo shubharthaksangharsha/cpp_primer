9cbdaf83ae14258bbf3ddeabb5e34e8ad917ecd6
shubharthaksangharsha <shubharthaksangharsha@gmai.com>
2021-06-13 17:59:23 +0530

    fixed the exercises

--- chapter3/iterators/3.22.cpp
+++ chapter3/iterators/3.22.cpp
@@ -1,19 +1,27 @@
 #include<iostream>
 #include<string>
 #include<cctype>
+#include<vector>
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
+using std::vector;
 
 int main(){
-  string word,c;
-  while(getline(cin,word)){
-    c = word;
+  string line;
+  vector<string> word;
+  while(cin>>line){
+    word.push_back(line);
   }
-  string::iterator first = c.begin();
-  for(; first != c.end() && !first->empty() ; first++){
-    *first=toupper(*first);
+  
+  vector<string>::iterator first = word.begin();
+  for(; first != word.end() && !first->empty() ; first++){
+
+    for(char &c  : *first) {
+      c= toupper(c);
+  }
+    cout<<*first<< " ";
   }
   
   return 0;
